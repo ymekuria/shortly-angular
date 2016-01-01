f5288bb181b5a9fbed42d3b627ca6e5a68478289
ymekuria <ymekuria@gmail.com>
2015-12-31 17:39:38 -0800

    passing 9 tests

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,11 +2,23 @@ angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
   // Your code here
-  var getAll = function(){};
+  var getAll = function () {
+    return $http({
+      method: 'GET',
+      url: '/api/links/'
+    })
+    .then(function(resp){
+      console.log("SD;FLKJASD;LKFJ;OIJROEJGHWRE");
+      return resp.data;
+    });
+  };
+
+  var addOne = function(){};
 
   return {
-    getAll: getAll
-  }
+    getAll: getAll,
+    addOne: addOne
+  };
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
